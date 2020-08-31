9f272c0150a952484f611b677528372af9a8c91c
mickeyr0124 <mickey@rose-trees.com>
2020-08-31 01:16:49 -0400

    Added SalesView and SalesViewModel
    Added WPF to SalesView and scaffolded SalesViewModel

--- TimCoRetailManager/TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TimCoRetailManager/TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls.Primitives;
 using TRMDesktopUI.Helpers;
+using TRMDesktopUI.Library.Api;
 
 namespace TRMDesktopUI.ViewModels
 {
@@ -42,6 +43,34 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+
+        public bool IsErrorVisible
+        {
+            get {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output; 
+            }
+
+            
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorVisible);
+            }
+        }
+
         public bool CanLogIn
         {
             get
@@ -61,12 +90,18 @@ namespace TRMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
+
+                //capture more information about the user
+                await _apiHelper.GetLoggedInUserInfo(result.Access_Token);
+
+
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
     }
