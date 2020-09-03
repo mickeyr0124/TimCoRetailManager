ef9dba35fb1380146cb4024ca92fb2aa9fc81e43
mickeyr0124 <mickey@rose-trees.com>
2020-09-03 00:58:46 -0400

    Add Taxable to Products

--- TimCoRetailManager/TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TimCoRetailManager/TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TRMDesktopUI.Library.Api;
+using TRMDesktopUI.Library.Helpers;
 using TRMDesktopUI.Library.Models;
 
 namespace TRMDesktopUI.ViewModels
@@ -14,9 +15,11 @@ namespace TRMDesktopUI.ViewModels
     {
 
         private IProductEndpoint _productEndPoint;
-        public SalesViewModel(IProductEndpoint productEndPoint)
+        IConfigHelper _configHelper;
+        public SalesViewModel(IProductEndpoint productEndPoint, IConfigHelper configHelper)
         {
             _productEndPoint = productEndPoint;
+            _configHelper = configHelper;
         }
 
         protected override async void OnViewLoaded(object view)
@@ -84,32 +87,52 @@ namespace TRMDesktopUI.ViewModels
         {
             get 
             {
-                decimal subTotal = 0;
+                return CalculateSubTotal().ToString("C");
+             }
+        }
 
-                foreach (var item in Cart)
+        private decimal CalculateSubTotal()
+        {
+            decimal subTotal = 0;
+
+            foreach (var item in Cart)
                 {
-                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                    subTotal += (item.Product.RetailPrice* item.QuantityInCart);
                 }
 
-                return subTotal.ToString("C"); 
-            }
-        }
+            return subTotal;
+}
+        private decimal CalculateTax()
+        {
+            decimal taxAmount = 0;
+            decimal taxRate = _configHelper.GetTaxRate()/100;
 
+            foreach (var item in Cart)
+            {
+                if (item.Product.IsTaxable)
+                {
+                    taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
+
+                }
+            }
+
+            return taxAmount;
+        }
         public string Tax
         {
-            get
-            {
-                //TODO - Replace with calculation
-                return "$0.00";
-            }
+           get
+                {
+                 return CalculateTax().ToString("C");
+                }        
         }
 
+ 
         public string Total
         {
             get
             {
-                //TODO - Replace with calculation
-                return "$0.00";
+                decimal total = CalculateSubTotal() + CalculateTax();
+                return total.ToString("C");
             }
         }
 
@@ -156,6 +179,8 @@ namespace TRMDesktopUI.ViewModels
             SelectedProduct.QuantityInStock -= ItemQuantity;
             ItemQuantity = 1;
             NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Tax);
+            NotifyOfPropertyChange(() => Total);
         }
 
         public bool CanRemoveFromCart
@@ -173,6 +198,9 @@ namespace TRMDesktopUI.ViewModels
         {
 
             NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Tax);
+            NotifyOfPropertyChange(() => Total);
+
         }
 
         public bool CanCheckOut
