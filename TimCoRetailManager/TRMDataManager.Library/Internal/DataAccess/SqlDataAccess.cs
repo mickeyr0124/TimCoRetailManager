5576feaa80af97167dd5d4aa80e78806d02156ff
mickeyr0124 <mickey@rose-trees.com>
2020-09-08 02:39:30 -0400

    Fix Transaction Exception Bug

--- TimCoRetailManager/TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TimCoRetailManager/TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -49,6 +49,8 @@ namespace TRMDataManager.Library.Internal.DataAccess
             _connection.Open();
 
             _transaction = _connection.BeginTransaction();
+
+            isClosed = false;
         }
 
         public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
@@ -70,22 +72,42 @@ namespace TRMDataManager.Library.Internal.DataAccess
             }
         }
 
+        private bool isClosed = false;
 
         public void CommitTransaction()
         {
             _transaction?.Commit();
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void RollbackTransaction()
         {
             _transaction?.Rollback();
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void Dispose()
         {
-            CommitTransaction();
+            if (isClosed == false)
+            {
+                try
+                {
+                    CommitTransaction(); 
+                }
+                catch
+                {
+
+                   // TODO: log this issue
+                }
+
+            }
+
+            _transaction = null;
+            _connection = null;
         }
 
         // Open connection.start transaction method
