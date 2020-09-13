f4cdd37c4287f3bd8cde2b4eb053dc9471e6a441
mickeyr0124 <mickey@rose-trees.com>
2020-09-13 00:46:53 -0400

    Upgrade both libraries to dot net standard

--- TimCoRetailManager/TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TimCoRetailManager/TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -43,7 +43,9 @@ namespace TRMDesktopUI.ViewModels
                 _selectedUser = value;
                 SelectedUserName = value.Email;
                 UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 LoadRoles();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 NotifyOfPropertyChange(() => SelectedUser);
             }
         }
